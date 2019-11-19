8d7de46d13859a912272fb95db9d5ccea40c18b4
Benjamin Rabe <brabe2@illinois.edu>
2019-11-19 15:29:37 -0600

    update to lr0 / lrf grid

--- gen_results.py
+++ gen_results.py
@@ -3,9 +3,9 @@ import os
 import pandas as pd
 from tensorboard.backend.event_processing import event_accumulator
 
-out_dir_base = "trials_p9/"
+out_dir_base = "trials/"
 with open(out_dir_base+"results.txt", 'w') as res_file:
-    res_file.write("INIT_LR,WEIGHT_DECAY,ACCURACY\n")
+    res_file.write("TRIAL,INIT_LR,WEIGHT_DECAY,ACCURACY\n")
     for trial in os.listdir(out_dir_base):
         if trial == 'results.txt':
             continue
@@ -17,4 +17,4 @@ with open(out_dir_base+"results.txt", 'w') as res_file:
         with open(out_dir+"/hyperparams.txt", 'r') as f:
             hyper = json.load(f)
         if hyper['global_batch_size'] == 512:
-            res_file.write("{},{},{}\n".format(hyper['lr0'], hyper['weight_decay'], acc))
+            res_file.write("{},{},{},{}\n".format(trial, hyper['lr0'], hyper['weight_decay'], acc))
--- main.py
+++ main.py
@@ -8,16 +8,16 @@ NUM_TRAIN_IMAGES = 1281167
 BATCH_SIZE = 512
 NUM_EPOCHS = 70
 LATE_LR_EPOCH = 4*NUM_EPOCHS//5
+WEIGHT_DECAY = 0.0004
 
 lr0_mult = 8
-wd_mult = 5
 lr0_space_min = 0.02
 lr0_space_max = 0.2
-wd_space_min = 0.00015
-wd_space_max = 0.0004
+lrf_space_min = lr0_space_min / 100
+lrf_space_max = lr0_space_max / 100
 lr0_space = list(np.geomspace(lr0_space_min, lr0_space_max, lr0_mult))
-wd_space = list(np.geomspace(wd_space_min, wd_space_max, wd_mult))
-combined_space = [(i,j) for i in list(lr0_space) for j in list(wd_space)]
+lrf_space = list(np.arange(lrf_space_min, lrf_space_max, 0.0001))
+combined_space = [(i,j) for i in list(lr0_space) for j in list(lrf_space)]
 
 model_dir_base = 'trials/'
 if not os.path.exists(model_dir_base):
@@ -25,14 +25,14 @@ if not os.path.exists(model_dir_base):
 trial = 0
 while os.path.exists(model_dir_base + str(trial)):
     trial += 1
-    if trial == len(combined_space):
+    # sometimes we go up, sometimes we go down
+    if trial >= len(combined_space) or trial < 0:
         print("All gridpts processed, exiting...")
         exit()
 model_dir = model_dir_base + str(trial) + '/'
 os.makedirs(model_dir)
 
-lr0, wd = combined_space[trial]
-lrf = lr0 / 100
+lr0, lrf = combined_space[trial]
 
 steps_per_epoch = ((NUM_TRAIN_IMAGES - 1 ) // BATCH_SIZE) + 1
 b = (-1 / (LATE_LR_EPOCH*steps_per_epoch))*np.log( lrf / lr0 )
@@ -40,6 +40,6 @@ b = (-1 / (LATE_LR_EPOCH*steps_per_epoch))*np.log( lrf / lr0 )
 exp = Experiment(model_dir=model_dir,
                  lr0=lr0,
                  lr_decay_rate=b,
-                 weight_decay=wd)
+                 weight_decay=WEIGHT_DECAY)
 exp.log_hyperparams()
 exp.execute()
